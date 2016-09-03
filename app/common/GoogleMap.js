88b1dc669e78d6ba9478a29694319839e5f8df18
timm <tnmyers@gmail.com>
2016-09-03 14:21:07 +0100

    GoogleMap class working

--- app/common/GoogleMap.js
+++ app/common/GoogleMap.js
@@ -1,10 +1,61 @@
 /*global google */
 const API = 'https://maps.googleapis.com/maps/api/js';
 
+/**
+ * Creates a Google Map from a postcode
+ */
+
 export default class GoogleMap {
 
 	constructor(parentEl, postCode) {
-		console.log('GoogleMap');
+
+		console.log('GoogleMap', parentEl, postCode);
+
+		this.rootEl = document.createElement('div');
+		this.rootEl.className = 'google-map';
+		this.rootEl.style.width = '100%';
+		this.rootEl.style.height = '100%';
+		parentEl.appendChild(this.rootEl);
+
+		// Add the script tag to download the API //
+		let remoteSrc = API;
+		let remote = document.createElement('script');
+				remote.onload = this.initMap.bind(this, this.rootEl, postCode);
+				remote.src = remoteSrc;
+				remote.setAttribute('async', true);
+				remote.setAttribute('defer', true);
+
+		document.body.appendChild(remote);
+	}
+
+	initMap(parentEl, postCode) {
+		// geocoder enables map from postcode, markers etc //
+		let geocoder = new google.maps.Geocoder();
+		let latlng = new google.maps.LatLng(0,0);
+		let mapOptions = {
+			zoom: 15,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		// init the map //
+		this.map = new google.maps.Map(parentEl, mapOptions);
+
+		// center on postcode and add marker //
+		geocoder.geocode( { 'address': postCode}, (results, status) => {
+
+			if (status == google.maps.GeocoderStatus.OK) {
+				this.map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+						map: this.map,
+						position: results[0].geometry.location
+				});
+			} else {
+				// errors //
+				alert('Could not create map: ' + status);
+			}
+		});
+
 	}
 
 }
--- app/map/MapView.js
+++ app/map/MapView.js
@@ -16,48 +16,7 @@ export default class MapView {
 		this.rootEl.className = 'map';
 		parentEl.appendChild(this.rootEl);
 
-//		let remoteSrc = API; // + '?key=' + API_KEY;// + '&callback=app.initMap%28%29';
-//		let remote = document.createElement('script');
-//				remote.onload = this.initGoogleMap.bind(this, this.rootEl);
-//				remote.src = remoteSrc;
-//				remote.setAttribute('async', true);
-//				remote.setAttribute('defer', true);
-//
-//		document.body.appendChild(remote);
-
+		// init the Google map //
 		this.gMap = new GoogleMap(this.rootEl, POSTCODE);
-
-	}
-
-	initGoogleMap(parentEl) {
-
-//		this.map = new google.maps.Map(parentEl, {
-//			center: {lat: -34.397, lng: 150.644},
-//			scrollwheel: false,
-//			zoom: 8
-//		});
-
-		var geocoder = new google.maps.Geocoder();
-		var latlng = new google.maps.LatLng(0,0);
-		var mapOptions = {
-			zoom: 15,
-			center: latlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-
-		this.map = new google.maps.Map(parentEl, mapOptions);
-
-		geocoder.geocode( { 'address': POSTCODE}, (results, status) => {
-			if (status == google.maps.GeocoderStatus.OK) {
-				this.map.setCenter(results[0].geometry.location);
-				var marker = new google.maps.Marker({
-						map: this.map,
-						position: results[0].geometry.location
-				});
-			} else {
-				alert('Geocode was not successful for the following reason: ' + status);
-			}
-		});
-
 	}
 }
