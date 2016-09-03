4c88678a1c34ee298c8de672e204fce6e30ec366
timm <tnmyers@gmail.com>
2016-09-03 13:29:37 +0100

    google map working

--- app/map/MapView.js
+++ app/map/MapView.js
@@ -1,3 +1,10 @@
+/*global google */
+const API = 'https://maps.googleapis.com/maps/api/js';
+
+const POSTCODE = 'WC1E 7BL';
+const DISTANCE = '3';
+const API_KEY = '';
+
 export default class MapView {
 
 	constructor(parentEl) {
@@ -6,5 +13,24 @@ export default class MapView {
 		this.rootEl = document.createElement('div');
 		this.rootEl.className = 'map';
 		parentEl.appendChild(this.rootEl);
+
+		let remoteSrc = API + '?key=' + API_KEY;// + '&callback=app.initMap%28%29';
+		let remote = document.createElement('script');
+				remote.onload = this.initGoogleMap.bind(this, this.rootEl);
+				remote.src = remoteSrc;
+				remote.setAttribute('async', true);
+				remote.setAttribute('defer', true);
+
+		document.body.appendChild(remote);
+	}
+
+	initGoogleMap(parentEl) {
+
+		this.map = new google.maps.Map(parentEl, {
+			center: {lat: -34.397, lng: 150.644},
+			scrollwheel: false,
+			zoom: 8
+		});
+
 	}
 }
